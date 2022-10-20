7ec41c2c72304ae47d3f3720234350450adb7751
Chris.c <cjs120dd@gmail.com>
2022-10-20 15:50:55 +0900

    feat:컴퓨터의 랜덤값을 생성한다

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const RANDOM_NUM_MIN = 1;
+const RANDOM_NUM_MAX = 9;
+
+export default class BaseballGame {
+    constructor() {
+        this.makeComputerInputNumbers();
+    }
+    
+    makeComputerInputNumbers(){
+        const inputNumberSet = new Set();
+        while (inputNumberSet.size < 3){
+        const randomNumber = MissionUtils.Random.pickNumberInRange(RANDOM_NUM_MIN, RANDOM_NUM_MAX);
+            inputNumberSet.add(randomNumber)
+        }
+        const computerInputNumbers = Number([...inputNumberSet].join(""));
+        console.log(randomNumber);
+        return computerInputNumbers;
+    }
+    
+    play(computerInputNumbers, userInputNumbers) {
+        return "결과 값 String";
+    }
+}
+
+new BaseballGame();
